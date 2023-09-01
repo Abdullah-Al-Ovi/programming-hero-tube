3b1e9adee38005c53630b26961f598741eb01816
Noob-Ovi <abdullah.ovi098@gmail.com>
2023-09-02 03:51:40 +0600

    Probably final touch

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
+ let isDescendingOrder = false;
+ let currenCategoryId = 1000;
  const handleCategory = async ()=>{
     const res = await fetch("https://openapi.programming-hero.com/api/videos/categories")
     const data = await res.json();
+    
     const categoryList=document.getElementById('categoryList');
-    // console.log(data.data[1].category);
+    
     data.data.forEach(category  =>{
-        // console.log(category.category);
+        
         const button = document.createElement('button')
         button.classList = "bg-gray-200 text-[252525B3] px-3 py-[2px] mr-3  rounded text-sm"
         button.innerHTML = `
             <button onclick=loadData(${category.category_id})>${category.category}</button>
         `
+       
         
         categoryList.appendChild(button)
     });
@@ -20,11 +24,13 @@
  const loadData = async(id)=>{
     const card = document.getElementById('cardSection')
     card.textContent=''
+    currenCategoryId = id;
     const res =await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
     const data = await res.json();
-    // console.log(id);
-    // console.log(data);
-    if(data.data.length===0){
+    const dataArr = data.data;
+    
+   
+    if(dataArr.length===0){
        const div=document.createElement('div') 
        div.classList="text-2xl  w-[80%] md:w-[90%] lg:w-[100%] my-20 mx-[41px] md:mx-[199px]  lg:mx-[440px] font-bold px-auto justify-centre items-centre "
        div.innerHTML = `
@@ -33,15 +39,23 @@
        `
        card.appendChild(div)
     }
+  
+
+    if(isDescendingOrder){
+      dataArr.sort((a,b)=>parseFloat(b.others.views.split(" ")[0])-parseFloat(a.others.views.split(" ")[0]));
+      
+      isDescendingOrder = !isDescendingOrder;
+    }
+  
     
-    data.data.forEach(content=>{
-        // console.log(content.others.posted_date);
+    
+    dataArr.forEach(content=>{
+        
         const time = content.others.posted_date;
         
         const hour = parseInt(time/3600)
         const minute = parseInt((time%3600)/60);
-        console.log(typeof time,typeof hour,typeof minute);
-        // console.log(parseInt(time/3600),parseInt((time%3600)/60));
+       
         const div = document.createElement('div')
     div.classList = "card card-compact bg-base-100  "
     div.innerHTML =`
@@ -70,6 +84,13 @@
     
  }
  handleCategory();
- loadData(1000)
+ loadData(currenCategoryId)
+ function sortCards(){
+   isDescendingOrder = !isDescendingOrder;
+   loadData(currenCategoryId)
+  
+ 
+
+ }
  
  
--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const goToBlog =()=>{
     localStorage.setItem('reload','true')
     window.location.href = "blog.html"
 }
-if(localStorage.getItem(reload)){
+if(localStorage.getItem("reload")){
     localStorage.removeItem('reload');
     location.reload;
 }
